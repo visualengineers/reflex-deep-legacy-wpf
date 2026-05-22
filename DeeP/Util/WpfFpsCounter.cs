e2ca1781c3733bdfc9af3b88ff01b5f4163263c3
Mathias Müller <mathias.mueller@visualistik.de>
2026-05-22 15:42:37 +0200

    fixed wpf frame stats

---
+++ DeeP/Util/WpfFpsCounter.cs
@@ -0,0 +1,41 @@
+﻿namespace ReFlex.Apps.DeeP.Util;
+
+using System;
+using System.Diagnostics;
+using System.Windows.Media;
+
+public sealed class WpfFpsCounter
+{
+    private readonly Stopwatch _watch = Stopwatch.StartNew();
+    private int _frames;
+
+    public double CurrentFps { get; private set; }
+    
+    public double TotalSeconds { get; private set; }
+    
+    public double TotalFrames { get; private set; }
+
+    public void Start()
+    {
+        CompositionTarget.Rendering += OnRendering;
+    }
+
+    public void Stop()
+    {
+        CompositionTarget.Rendering -= OnRendering;
+    }
+
+    private void OnRendering(object? sender, EventArgs e)
+    {
+        _frames++;
+
+        if (_watch.Elapsed.TotalSeconds >= 1)
+        {
+            CurrentFps = _frames / _watch.Elapsed.TotalSeconds;
+            _frames = 0;
+            _watch.Restart();
+
+            Debug.WriteLine($"WPF FPS: {CurrentFps:F1}");
+        }
+    }
+}
